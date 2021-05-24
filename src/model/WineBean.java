7f41d7c296b45d927386c884f67b57e842034539
Giovanni Prisco <gprisco01@gmail.com>
2021-05-24 18:58:21 +0200

    Added Cart

--- src/model/WineBean.java
+++ src/model/WineBean.java
@@ -86,4 +86,26 @@ public class WineBean {
 	public void addWinegrape(WineWinegrapeBean winegrape) {
 		winegrapes.add(winegrape);
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		// If the object is compared with itself then return true
+		if (o == this) {
+			return true;
+		}
+
+		/*
+		 * Check if o is an instance of Complex or not "null instanceof [type]" also
+		 * returns false
+		 */
+		if (!(o instanceof WineBean)) {
+			return false;
+		}
+
+		// typecast o to Complex so that we can compare data members
+		WineBean w = (WineBean) o;
+
+		// Compare the data members and return accordingly
+		return this.getPk().equals(w.getPk());
+	}
 }
--- src/model/WinePrimaryKey.java
+++ src/model/WinePrimaryKey.java
@@ -16,4 +16,26 @@ public final class WinePrimaryKey {
 	public int getVintage() {
 		return vintage;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		// If the object is compared with itself then return true
+		if (o == this) {
+			return true;
+		}
+
+		/*
+		 * Check if o is an instance of Complex or not "null instanceof [type]" also
+		 * returns false
+		 */
+		if (!(o instanceof WinePrimaryKey)) {
+			return false;
+		}
+
+		// typecast o to Complex so that we can compare data members
+		WinePrimaryKey w = (WinePrimaryKey) o;
+
+		// Compare the data members and return accordingly
+		return this.getWine().equals(w.getWine()) && this.getVintage() == w.getVintage();
+	}
 }
